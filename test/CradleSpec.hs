3aa1e65e2d856f897f03e04476768628712067c3
Sönke Hahn <soenkehahn@gmail.com>
2023-06-21 20:52:35 -0400

    Add more tests for exceptions

--- test/CradleSpec.hs
+++ test/CradleSpec.hs
@@ -27,6 +27,27 @@ spec = do
                             && ioe_type e == NoSuchThing
                       )
 
+    it "throws when the executable flag isn't set" $ do
+      writeFile "exe" "whatever"
+      run "./exe"
+        `shouldThrow` ( \(e :: IOException) ->
+                          ( ioe_filename e == Just "./exe"
+                              && ioe_type e == PermissionDenied
+                          )
+                      )
+
+    it "throws when the hashbang interpreter cannot be found" $ do
+      writeFile "exe" "#!/does/not/exist\nfoo"
+      makeExecutable "exe"
+      run "./exe"
+        `shouldThrow` ( \(e :: IOException) ->
+                          ioe_filename e == Just "./exe"
+                            && ioe_type e == NoSuchThing
+                      )
+
+    it "throws when the exitcode is not 0" $ do
+      pending
+
 makeExecutable :: FilePath -> IO ()
 makeExecutable file = do
   permissions <- getPermissions file
