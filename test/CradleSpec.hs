4b24c7fb8e8626bc82e0fe83101c999849ddf79d
Sönke Hahn <soenkehahn@gmail.com>
2023-06-22 01:15:37 -0400

    Restructure tests

--- test/CradleSpec.hs
+++ test/CradleSpec.hs
@@ -19,35 +19,36 @@ spec = do
       run_ "./exe"
       doesFileExist "file" `shouldReturn` True
 
-    it "throws when executable cannot be found" $ do
-      run_ "./exe"
-        `shouldThrow` ( \(e :: IOException) ->
-                          ioe_filename e == Just "./exe"
-                            && ioe_type e == NoSuchThing
-                      )
+    describe "exceptions" $ do
+      it "throws when executable cannot be found" $ do
+        run_ "./exe"
+          `shouldThrow` ( \(e :: IOException) ->
+                            ioe_filename e == Just "./exe"
+                              && ioe_type e == NoSuchThing
+                        )
 
-    it "throws when the executable flag isn't set" $ do
-      writeFile "exe" "whatever"
-      run_ "./exe"
-        `shouldThrow` ( \(e :: IOException) ->
-                          ( ioe_filename e == Just "./exe"
-                              && ioe_type e == PermissionDenied
-                          )
-                      )
+      it "throws when the executable flag isn't set" $ do
+        writeFile "exe" "whatever"
+        run_ "./exe"
+          `shouldThrow` ( \(e :: IOException) ->
+                            ( ioe_filename e == Just "./exe"
+                                && ioe_type e == PermissionDenied
+                            )
+                        )
 
-    it "throws when the hashbang interpreter cannot be found" $ do
-      writeFile "exe" "#!/does/not/exist\nfoo"
-      makeExecutable "exe"
-      run_ "./exe"
-        `shouldThrow` ( \(e :: IOException) ->
-                          ioe_filename e == Just "./exe"
-                            && ioe_type e == NoSuchThing
-                      )
+      it "throws when the hashbang interpreter cannot be found" $ do
+        writeFile "exe" "#!/does/not/exist\nfoo"
+        makeExecutable "exe"
+        run_ "./exe"
+          `shouldThrow` ( \(e :: IOException) ->
+                            ioe_filename e == Just "./exe"
+                              && ioe_type e == NoSuchThing
+                        )
 
-    it "throws when the exitcode is not 0" $ do
-      writeBashScript "exe" "exit 42"
-      run_ "./exe"
-        `shouldThrowShow` "command failed with exitcode 42: ./exe"
+      it "throws when the exitcode is not 0" $ do
+        writeBashScript "exe" "exit 42"
+        run_ "./exe"
+          `shouldThrowShow` "command failed with exitcode 42: ./exe"
 
     it "allows to be run in MonadIO contexts" $ do
       writeBashScript "exe" "true"
@@ -55,47 +56,49 @@ spec = do
         _ :: String <- run "./exe"
         run_ "./exe"
 
-    it "allows to pass in an argument" $ do
-      writeBashScript "exe" "echo $1 > file"
-      run_ ("./exe", "foo")
-      readFile "file" `shouldReturn` "foo\n"
+    describe "arguments" $ do
+      it "allows to pass in an argument" $ do
+        writeBashScript "exe" "echo $1 > file"
+        run_ ("./exe", "foo")
+        readFile "file" `shouldReturn` "foo\n"
 
-    it "allows to pass in multiple arguments" $ do
-      writeBashScript "exe" "echo $1 > file; echo $2 >> file"
-      run_ ("./exe", ["foo", "bar"])
-      readFile "file" `shouldReturn` "foo\nbar\n"
+      it "allows to pass in multiple arguments" $ do
+        writeBashScript "exe" "echo $1 > file; echo $2 >> file"
+        run_ ("./exe", ["foo", "bar"])
+        readFile "file" `shouldReturn` "foo\nbar\n"
 
-    it "provides nice syntax for multiple arguments" $ do
-      writeBashScript "exe" "echo $1 > file"
-      run_ "./exe" "foo"
-      readFile "file" `shouldReturn` "foo\n"
-      writeBashScript "exe" "echo $1 > file; echo $2 >> file"
-      run_ "./exe" "foo" "bar"
-      readFile "file" `shouldReturn` "foo\nbar\n"
+      it "provides nice syntax for multiple arguments" $ do
+        writeBashScript "exe" "echo $1 > file"
+        run_ "./exe" "foo"
+        readFile "file" `shouldReturn` "foo\n"
+        writeBashScript "exe" "echo $1 > file; echo $2 >> file"
+        run_ "./exe" "foo" "bar"
+        readFile "file" `shouldReturn` "foo\nbar\n"
 
-    it "allows to capture stdout" $ do
-      writeBashScript "exe" "echo output"
-      stdout <- run "./exe"
-      stdout `shouldBe` "output\n"
+    describe "capturing stdout" $ do
+      it "allows to capture stdout" $ do
+        writeBashScript "exe" "echo output"
+        stdout <- run "./exe"
+        stdout `shouldBe` "output\n"
 
-    it "allows to capture stdout *and* pass in arguments" $ do
-      writeBashScript "exe" "echo $1"
-      output <- run "./exe" "foo"
-      output `shouldBe` "foo\n"
-      writeBashScript "exe" "echo $1; echo $2"
-      output <- run "./exe" "foo" "bar"
-      output `shouldBe` "foo\nbar\n"
+      it "allows to capture stdout *and* pass in arguments" $ do
+        writeBashScript "exe" "echo $1"
+        output <- run "./exe" "foo"
+        output `shouldBe` "foo\n"
+        writeBashScript "exe" "echo $1; echo $2"
+        output <- run "./exe" "foo" "bar"
+        output `shouldBe` "foo\nbar\n"
 
-    describe "StdoutTrimmed" $ do
-      it "allows to capture the stripped stdout" $ do
-        writeBashScript "exe" "echo foo"
-        StdoutTrimmed output <- run "./exe" "foo"
-        output `shouldBe` "foo"
+      describe "StdoutTrimmed" $ do
+        it "allows to capture the stripped stdout" $ do
+          writeBashScript "exe" "echo foo"
+          StdoutTrimmed output <- run "./exe" "foo"
+          output `shouldBe` "foo"
 
-      it "strips leading and trailing spaces and newlines" $ do
-        writeBashScript "exe" "echo '  foo   '"
-        StdoutTrimmed output <- run "./exe" "foo"
-        output `shouldBe` "foo"
+        it "strips leading and trailing spaces and newlines" $ do
+          writeBashScript "exe" "echo '  foo   '"
+          StdoutTrimmed output <- run "./exe" "foo"
+          output `shouldBe` "foo"
 
 writeBashScript :: FilePath -> String -> IO ()
 writeBashScript file code = do
