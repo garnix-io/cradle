c96c21dbcbb6e8690585b916f795f3eb1fc4d660
Sönke Hahn <soenkehahn@gmail.com>
2023-06-22 01:28:10 -0400

    Introduce Cradle.Input submodule

--- src/Cradle.hs
+++ src/Cradle.hs
@@ -10,16 +10,17 @@
 
 module Cradle
   ( module Cradle,
+    module Cradle.Input,
     module Cradle.Output,
   )
 where
 
 import Control.Exception (ErrorCall (..), throwIO)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Cradle.Input
 import Cradle.Output
 import Cradle.ProcessConfiguration
 import Data.Kind
-import Data.List (foldl')
 import GHC.TypeError (ErrorMessage (..), TypeError)
 
 run_ ::
@@ -43,39 +44,18 @@ class RunProcessConfig runnable where
   runProcessConfig :: Maybe ProcessConfiguration -> runnable
 
 instance
-  (ProcessOption option, RunProcessConfig runnable) =>
-  RunProcessConfig (option -> runnable)
+  (Input input, RunProcessConfig runnable) =>
+  RunProcessConfig (input -> runnable)
   where
-  runProcessConfig :: Maybe ProcessConfiguration -> option -> runnable
-  runProcessConfig createProcess option =
-    runProcessConfig (configureProcess option createProcess)
+  runProcessConfig :: Maybe ProcessConfiguration -> input -> runnable
+  runProcessConfig createProcess input =
+    runProcessConfig (configureProcess input createProcess)
 
 instance {-# OVERLAPS #-} (MonadIO m, Output a) => RunProcessConfig (m a) where
   runProcessConfig :: Maybe ProcessConfiguration -> m a
   runProcessConfig = \case
     Nothing -> liftIO $ throwIO $ ErrorCall "should be impossible, see DefinesExecutable"
     Just config -> liftIO $ runAndGetOutput config
-
-class ProcessOption option where
-  configureProcess :: option -> Maybe ProcessConfiguration -> Maybe ProcessConfiguration
-
-instance ProcessOption String where
-  configureProcess :: String -> Maybe ProcessConfiguration -> Maybe ProcessConfiguration
-  configureProcess s = \case
-    Nothing ->
-      Just $ ProcessConfiguration s []
-    Just (ProcessConfiguration exe args) ->
-      Just $ ProcessConfiguration exe (args ++ [s])
-
-instance ProcessOption () where
-  configureProcess () = id
-
-instance (ProcessOption a, ProcessOption b) => ProcessOption (a, b) where
-  configureProcess (a, b) = configureProcess b . configureProcess a
-
-instance {-# OVERLAPS #-} (ProcessOption option) => ProcessOption [option] where
-  configureProcess list config =
-    foldl' (flip configureProcess) config list
 
 type family Check (bool :: Bool) :: Constraint where
   Check True = ()
@@ -86,15 +66,15 @@ type family Check (bool :: Bool) :: Constraint where
       )
 
 type family DefinesExecutable (runnable :: Type) :: Bool where
-  DefinesExecutable (option -> runnable) =
-    ContainsExecutable option || DefinesExecutable runnable
+  DefinesExecutable (input -> runnable) =
+    ContainsExecutable input || DefinesExecutable runnable
   DefinesExecutable runnable = False
 
-type family ContainsExecutable (option :: Type) :: Bool where
+type family ContainsExecutable (input :: Type) :: Bool where
   ContainsExecutable String = True
   ContainsExecutable (a, b) =
     ContainsExecutable a || ContainsExecutable b
-  ContainsExecutable option = False
+  ContainsExecutable input = False
 
 type family (||) (a :: Bool) (b :: Bool) :: Bool where
   False || False = False
---
+++ src/Cradle/Input.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Cradle.Input where
+
+import Cradle.ProcessConfiguration
+import Data.List
+
+class Input input where
+  configureProcess :: input -> Maybe ProcessConfiguration -> Maybe ProcessConfiguration
+
+instance Input String where
+  configureProcess :: String -> Maybe ProcessConfiguration -> Maybe ProcessConfiguration
+  configureProcess s = \case
+    Nothing ->
+      Just $ ProcessConfiguration s []
+    Just (ProcessConfiguration exe args) ->
+      Just $ ProcessConfiguration exe (args ++ [s])
+
+instance Input () where
+  configureProcess () = id
+
+instance (Input a, Input b) => Input (a, b) where
+  configureProcess (a, b) = configureProcess b . configureProcess a
+
+instance {-# OVERLAPS #-} (Input input) => Input [input] where
+  configureProcess list config =
+    foldl' (flip configureProcess) config list
