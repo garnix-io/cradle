e90551ce88e0c9f972b9f2f8d7e96827bffbdd7c
Your Name <you@example.com>
2023-07-17 10:19:00 -0400

    Add some documentation

--- src/Cradle.hs
+++ src/Cradle.hs
@@ -10,10 +10,14 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module Cradle
-  ( module Cradle,
+  ( run,
+    run_,
+    Runnable,
+
+    -- * Possible Output
     Output,
-    Input,
     StdoutTrimmed (..),
+    ExitCode (..),
   )
 where
 
@@ -23,13 +27,8 @@ import Cradle.Input
 import Cradle.Output
 import Cradle.ProcessConfiguration
 import Data.Kind
-import Data.Proxy
 import GHC.TypeError (ErrorMessage (..), TypeError)
-
-run_ ::
-  (Runnable runnable, ReturnsUnit runnable) =>
-  runnable
-run_ = run
+import System.Exit (ExitCode (..))
 
 class ReturnsUnit a
 
@@ -40,8 +39,59 @@ instance {-# OVERLAPPABLE #-} (Monad m, a ~ ()) => ReturnsUnit (m a)
 type Runnable runnable =
   (RunProcessConfig runnable, Check (DefinesExecutable runnable))
 
+-- | Runs a child process and retrieves a result from it.
+--
+-- For example:
+--
+-- >>> stdout :: String <- run "echo" "Hello, World!"
+-- >>> print stdout
+-- "Hello, World!\n"
+--
+-- == Outputs
+--
+-- `run` is polymorphic in its output, the output type just has to implement 'Output'.
+-- So for example you can get the exit code of a process like this:
+--
+-- >>> run "false" :: IO ExitCode
+-- ExitFailure 1
+--
+-- If you don't want to retrieve any information from a child process, you can use `run_` (or make the output type '()').
+--
+-- For more information on available output types, see 'Output'.
+--
+-- == Inputs
+--
+-- 'run' and 'run_' are also variadic, i.e. it takes as many arguments as you want.
+-- All arguments are polymorphic and have to implement 'Input'.
+--
+-- >>> run_ "echo" "foo" "bar"
+-- foo bar
+-- >>> run_ "echo"
+-- <BLANKLINE>
+--
+-- == No Automatic Splitting of Strings, No Shells
+--
+-- `cradle` will not split any inputs by whitespace. So e.g. this doesn't work:
+--
+-- >>> run_ "echo foo"
+-- *** Exception: echo foo: createProcess: posix_spawnp: does not exist (No such file or directory)
+--
+-- This is trying to run an executable with the file name @"echo foo"@, which doesn't exist.
+-- You can split words in haskell though:
+--
+-- >>> run_ "echo" (words "foo bar")
+-- foo bar
 run :: (Runnable runnable) => runnable
 run = runProcessConfig Nothing
+
+-- | Same as `run`, but always returns '()'.
+--
+-- >>> run_ "echo" "Hello, World!"
+-- Hello, World!
+run_ ::
+  (Runnable runnable, ReturnsUnit runnable) =>
+  runnable
+run_ = run
 
 class RunProcessConfig runnable where
   runProcessConfig :: Maybe ProcessConfiguration -> runnable
