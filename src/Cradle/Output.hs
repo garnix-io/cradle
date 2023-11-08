f8cd3a0ad4f7cbcdcb0e8ef8edc572ed5b7db2a4
Sönke Hahn <soenkehahn@gmail.com>
2023-11-07 23:06:52 -0500

    Implement up to 6-tuples for Output

--- src/Cradle/Output.hs
+++ src/Cradle/Output.hs
@@ -29,18 +29,85 @@ instance Output () where
   configure Proxy = id
   extractOutput = const ()
 
-instance (Output a, Output b) => Output (a, b) where
+instance
+  (Output a, Output b) =>
+  Output (a, b)
+  where
   configure Proxy =
     configure (Proxy :: Proxy a)
       >>> configure (Proxy :: Proxy b)
-  extractOutput result = (extractOutput result, extractOutput result)
+  extractOutput result =
+    ( extractOutput result,
+      extractOutput result
+    )
 
-instance (Output a, Output b, Output c) => Output (a, b, c) where
+instance
+  (Output a, Output b, Output c) =>
+  Output (a, b, c)
+  where
   configure Proxy =
     configure (Proxy :: Proxy a)
       >>> configure (Proxy :: Proxy b)
       >>> configure (Proxy :: Proxy c)
-  extractOutput result = (extractOutput result, extractOutput result, extractOutput result)
+  extractOutput result =
+    ( extractOutput result,
+      extractOutput result,
+      extractOutput result
+    )
+
+instance
+  (Output a, Output b, Output c, Output d) =>
+  Output (a, b, c, d)
+  where
+  configure Proxy =
+    configure (Proxy :: Proxy a)
+      >>> configure (Proxy :: Proxy b)
+      >>> configure (Proxy :: Proxy c)
+      >>> configure (Proxy :: Proxy d)
+  extractOutput result =
+    ( extractOutput result,
+      extractOutput result,
+      extractOutput result,
+      extractOutput result
+    )
+
+instance
+  (Output a, Output b, Output c, Output d, Output e) =>
+  Output (a, b, c, d, e)
+  where
+  configure Proxy =
+    configure (Proxy :: Proxy a)
+      >>> configure (Proxy :: Proxy b)
+      >>> configure (Proxy :: Proxy c)
+      >>> configure (Proxy :: Proxy d)
+      >>> configure (Proxy :: Proxy e)
+  extractOutput result =
+    ( extractOutput result,
+      extractOutput result,
+      extractOutput result,
+      extractOutput result,
+      extractOutput result
+    )
+
+instance
+  (Output a, Output b, Output c, Output d, Output e, Output f) =>
+  Output (a, b, c, d, e, f)
+  where
+  configure Proxy =
+    configure (Proxy :: Proxy a)
+      >>> configure (Proxy :: Proxy b)
+      >>> configure (Proxy :: Proxy c)
+      >>> configure (Proxy :: Proxy d)
+      >>> configure (Proxy :: Proxy e)
+      >>> configure (Proxy :: Proxy f)
+  extractOutput result =
+    ( extractOutput result,
+      extractOutput result,
+      extractOutput result,
+      extractOutput result,
+      extractOutput result,
+      extractOutput result
+    )
 
 newtype StdoutUntrimmed = StdoutUntrimmed
   { fromStdoutUntrimmed :: ByteString
